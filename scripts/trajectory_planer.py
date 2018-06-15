964cc417a6c586a4db9df92b117f40424d8e5a04
Lukas Huber <hubernikus@gmail.com>
2018-06-15 16:09:39 +0200

    create it

--- scripts/trajectory_planer.py
+++ scripts/trajectory_planer.py
@@ -8,92 +8,209 @@
 #
 #'''
 
-
+# Custom libraries
 import sys 
-lib_string = "/home/lukas/catkin_ws/src/obstacle_recognition/scripts/"
+lib_string = "/home/lukas/catkin_ws/src/obstacle_recognition/scripts/lib/"
 if not any (lib_string in s for s in sys.path):
     sys.path.append(lib_string)
 
+from lib_modulation import *
+from lib_ds import *
+
+# ROS tools    
 import rospy
 
-from obstacle_recognition.msg import Obstacle
-from trajectory_msgs.msg import JointTrajectory
-
-import numpy as np
-from math import pi, floor
+from obstacle_recognition.msg import Obstacle # Custom message
+from geometry_msgs.msg import Point, Quaternion, Pose, PoseStamped
+from nav_msgs.msg import Path
 
 import tf
 
+# MATH 
+import numpy as np
+from math import pi, floor
+
+import copy # copying of lists
+
+
 class TrajectoryPlanner():
     def __init__(self):
+        print('Start node')
         # Initialize node
         rospy.init_node('ellipse_publisher', anonymous=True)
-        rate = rospy.Rate(20) # Frequency
+        rate = rospy.Rate(2) # Frequency
 
+        self.n_obs = 1
         # Create publishers
-        pub_traj = rospy.Publisher('ds_finalTrajectory', JointTrajectory, queue_size=5)
+        pub_traj = rospy.Publisher('ds_finalTrajectory', Path, queue_size=5)
+        
+        pub_pos1 = rospy.Publisher('pose_obstacle', PoseStamped, queue_size=5)
 
         # Create listener
-        pose_sub = rospy.Subscriber("object_2/pose", Obstacle, self.callback)
-        listener = tf.TransformListener()
+        pose_sub = [0,0]
+        pose_sub[0] = rospy.Subscriber("obstacle0", Obstacle, self.callback_ob0)
+        pose_sub[1] = rospy.Subscriber("obstacle1", Obstacle, self.callback_ob1)
+        attr_sub = rospy.Subscriber("attr", Obstacle, self.callback_ob1)
+        self.listener = tf.TransformListener() # TF listener
 
-        self.obs = []
+        self.n_obs = 1
+        self.obs = [0]*self.n_obs
+        self.pos_obs=[0]*self.n_obs
+        self.quat_ob=[0]*self.n_obs
+
+        print('wait obstacle')
+        self.awaitObstacle = [True for i in range(self.n_obs)]
+        while any(self.awaitObstacle):
+            rospy.sleep(0.1)  # Wait zzzz* = 1 # wait
+        print('got it')
+
+        # Get initial transformation
+        rospy.loginfo('Getting Transformationss.')
+        awaitingTrafo_ee=True
+        awaitingTrafo_obs[i]= [True] * self.obs    
+        awaitingTrafo_attr=True
+
+        for i in len(self.obs):
+            while(awaitingTrafo_obs[i]): # Trafo obs1 position
+                try:
+                    self.pos_obs[i], self.quat_obs[i] = self.listener.lookupTransform("/mocap_world", "/object_{}/base_link".format(i), rospy.Time(0))                   
+                    awaitingTrafo_obs[i]=False
+                except:
+                    rospy.loginfo('Waiting for TRAFO at {}'.format(rospy.get_time()))
+                    rospy.sleep(0.1)  # Wait zzzz*
+
+        while(awaitingTrafo_ee): # TRAFO robot position
+            try: # Get transformation
+                self.pos_rob, self.pos_rob = self.listener.lookupTransform("/mocap_world", "/lwr_7_link", rospy.Time(0))
+                awaitingTrafo_ee=False
+            except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
+                rospy.loginfo('Waiting for TRAFO')
+                rospy.sleep(0.1)  # Wait zzzz*
+            
+        while(awaitingTrafo_attr): # Trafo obs2 position
+            try:
+                self.pos_attr, self.quat_attr = self.listener.lookupTransform("/mocap_world", "/object_2/base_link", rospy.Time(0))                   
+                awaitingTrafo_attr=False
+            except:
+                rospy.loginfo("Waiting for TRAFO at {}".format(rospy.get_time()))
+                rospy.sleep(0.5)  # Wait zzzz*
+                
+        rospy.loginfo("All TF recieved")
+
+        self.iSim = 0
+
+        # Variables for trajectory prediciton
+        self.n_intSteps = 100
+        self.dt = 0.05
+        self.dim = 3 #  3D space
 
         while not rospy.is_shutdown():
-            rospy.loginfo("Publishing ellipse %s" % rospy.get_time())
-
-            # Variables for trajectory prediciton
-            self.n_integration = 1000
-            self.dt = 0.01
-            self.dim = 3 #  3D space
+            try: # Get transformation
+                self.pos_rob, self.quat_rob = self.listener.lookupTransform("/mocap_world", "/lwr_7_link", rospy.Time(0))                   
+            except:
+                rospy.loginfo("No <</lwr_7_link>> recieved")
+                #continue
+            x0_lwr7 = np.array([0,0,0])+np.array(self.pos_rob)
 
             try: # Get transformation
-                (trans,rot) = listener.lookupTransform('/object_1/base_link', '/lwr_7_link', rospy.Time(0))
-            except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
-                continue
-
-            # Is at origin of base_link 2 - therefore no initial pos
-            x0_lwr7 = np.array([0,0,0])
-
-            x_attractor = np.array([0,0,0])
-            x_attractor = x_attractor + np.array(trans) # Transform to lwr_7_link
+                    self.pos_attr, self.attr = self.listener.lookupTransform( "/mocap_world", "/object_1/base_link", rospy.Time(0))                   
+            except:
+                rospy.loginfo("No <<object2>> recieved")
+                #continue
+            x_attractor = np.array([0,0,0]) + np.array(self.pos_attr) # Transform to lwr_7_link
             
-            traj = JointTrajectory()
+            obs_roboFrame = copy.deepcopy(self.obs)  # TODO -- change, because only reference is created not copy...
+            for n in range(len(self.obs)): # for all bostacles
+                try: # Get transformation
+                    self.pos_obs[n], self.quat_obs[n] = self.listener.lookupTransform("/mocap_world", "/object_{}/base_link".format(n), rospy.Time(0))                   
+                except:# (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
+                    rospy.loginfo("No <<object1>> recieved")
+                    #continue
+
+                quat_thr = tf.transformations.quaternion_from_euler(self.obs[n].th_r[0],
+                                                                    self.obs[n].th_r[1],
+                                                                    self.obs[n].th_r[2])
+                quat_thr_mocap = tf.transformations.quaternion_multiply(self.quat_ob1, quat_thr)
+
+                th_r_mocap = tf.transformations.euler_from_quaternion(quat_thr_mocap)
+
+                # TODO apply transofrm to x0
+                #q_x0 = tf.transformations.quaternion_multiply( self.quat_ob1, np.hstack(( self.obs[n].x0, 0)))
+                #q_x0 = tf.transformations.quaternion_multiply( np.hstack((self.obs[n].x0, 0)), self.quat_ob1)
+                #obs_roboFrame[n].x0 = q_x0[0:3] + np.array(self.pos_ob1) # Transpose into reference frame
+                obs_roboFrame[n].x0 = self.ob[n].x0 + np.array(self.pos_ob[n]) # Transpose into reference frame
+                
+                # TODOOOOOOOOOOOOO --- activate
+                obs_roboFrame[n].th_r =  [th_r_mocap[0],
+                                          th_r_mocap[1],
+                                          th_r_mocap[2]]# Rotate into reference frame
+
+                pose_obs = PoseStamped() # Publish pose for verification
+                pose_obs.header.stamp = rospy.Time.now()
+                pose_obs.header.frame_id = '/mocap_world'
+                pose_obs.pose.orientation = Quaternion(quat_thr_mocap[0],
+                                                      quat_thr_mocap[1],
+                                                      quat_thr_mocap[2],
+                                                      quat_thr_mocap[3])
+                
+                pose_obs.pose.position = Point(obs_roboFrame[n].x0[0],
+                                              obs_roboFrame[n].x0[1],
+                                              obs_roboFrame[n].x0[2])
+                pub_pos1.publish(pose_obs)
+                
+            traj = Path()
+            traj.header.stamp = rospy.Time.now()
+            traj.header.frame_id = '/mocap_world'
             
-            self.pos = np.array((self.dim, self.n_integration))
-                    for iSim in range(self.n_intSteps):
-                        ds_init = linearAttractor_const(x, x0=x_attractor)
-                        ds_modulated = obs_avoidance_convergence(x, ds_init, self.obs)
-                        x = ds_modulted*dt + x
+            q0 = Quaternion(1, 0, 0, 0) # Unit quaternion for trajectory
 
-                        traj.points.positions.x = x[0]
-                        traj.points.positions.y = x[1]
-                        traj.points.positions.z = x[2]
-                        
-        traj = JointTrajectory()
-        traj.header.time = rospy.Time.now()
-        traj.frame_id = '/lwr_7_link'
+            x = x0_lwr7 # x for trajectory creating
+            x_hat =  x0_lwr7 # x fro linear DS
 
-        pub_traj.publish(traj)
+            # print('WARNING -- ERROR in obs[n].w calculation')
+            # print(obs_roboFrame[0])
+            obs_roboFrame[n].center_dyn = obs_roboFrame[n].x0
+            
+            obs_roboFrame[0].w = [0,0,0]
+            obs_roboFrame[0].xd = [0,0,0]
+            
+            #obs_roboFrame = []
+            loopCount = 0
+            for iSim in range(self.n_intSteps):
+                ds_init = linearAttractor_const(x, x0=x_attractor)
+                ds_modulated = obs_avoidance_convergence(x, ds_init, obs_roboFrame)
+                
+                x = ds_modulated*self.dt + x
+                x_hat = ds_init*self.dt + x_hat
+                #x=x_hat
+                
+                # Add to trajectory
+                pose = PoseStamped()
+                pose.header = traj.header
+                pose.pose.position = Point(x[0],x[1],x[2])
+                #pose.pose.position = Point(x_hat[0],x_hat[1],x_hat[2])
+                pose.pose.orientation = q0
+                traj.poses.append(pose)
+
+                loopCount += 1
+                
+            pub_traj.publish(traj)
+            rospy.loginfo("Publishing Trajectory %s" % rospy.get_time())
+            
+            self.iSim += 1
+            
+            rate.sleep()
         
         
-    def callback(self,msg):
+    def callback_ob0(self,msg):
         i =0 # TODO add second obstacle
-        obs[i] = msg
+        self.awaitObstacle[i] = False
+        self.obs[i] = msg
 
-        
-def linearAttractor_const(x, x0 = 'default', velConst=6):
-    # change initial value for n dimensions
-    # TODO -- constant velocity // maximum velocity
-    dim, M = x.shape
-
-    if x0=='default':
-        x0 = dim*[0]
-    xd = - (x-np.tile(x0,M))
-    
-    return xd
-
-            
+    def callback_ob1(self,msg):
+        i =1 # TODO add second obstacle
+        self.awaitObstacle[i] = False
+        self.obs[i] = msg
  
 if __name__ == '__main__':
     try:
--- src/transform_creator.cpp
+++ src/transform_creator.cpp
@@ -1,47 +1,65 @@
 #include "ros/ros.h"
-
 #include <tf/transform_broadcaster.h>
-
 #include "geometry_msgs/PoseStamped.h"
-
+#include "geometry_msgs/Pose.h"
 #include "std_msgs/String.h"
-
 #include <sstream>
 
+#include <iostream>
+
+const float filtCoeff = 0.999;
+bool firstTFrecieved = false;
+//tf::PoseStamped pose_filtered;
+geometry_msgs::Pose pose_filtered;
+
+
 //void (const std_msgs::String::ConstPtr& msg)
-void obj1_callback(const geometry_msgs::PoseStamped& pose_robo1)
-{
-  static tf::TransformBroadcaster obs1_br;
-  tf::Transform transform;
-//  transform.position = pose_robo1.Pose.position;
- 
-  float aa;
-  std::cout << "hello you" << aa << "me too \n";
-  //aa = pose_robo1->pose.position.x;
-  //transform.setOrigin(pose_robo1->pose.position);
-  //transform.orientation = pose_robo1.Pose.orientation;
+void obj1_callback(const geometry_msgs::PoseStamped& pose_robo1){
+  // pose_filtered.position.x = pose_filtered.position.x*filtCoeff + (1.-filtCoeff)*transform.pose_robo1.position.x;
   //obs1_br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "world", "base_link_obs1"));
 
   static tf::TransformBroadcaster mocap_br;
-  //transform.setOrigin(tf::Vector3(0.0,0.0,-1.2));
-  transform.setOrigin(tf::Vector3(pose_robo1.pose.position.x,
-				  pose_robo1.pose.position.y,
-				  pose_robo1.pose.position.z
-				  ) );
-  //tf::Quaternion quat = tf::createQuaternionFromRPY(0, 0,0); 
-  tf::Quaternion quat = tf::Quaternion(pose_robo1.pose.orientation.x,
-				       pose_robo1.pose.orientation.y,
-				       pose_robo1.pose.orientation.z,
-				       pose_robo1.pose.orientation.w);
+  tf::Quaternion quat;
+  tf::Transform transform;
 
-  //transform.setRotation(quat);
-  //quat = pose_robo1.pose.orientation;
+  float k;
+  if(firstTFrecieved){
+    k = filtCoeff;
+  }
+  else{
+    k=0;
+    firstTFrecieved = true;
+  }
+  
+  // Filter position
+  pose_filtered.position.x = pose_filtered.position.x*k + (1.-k)*pose_robo1.pose.position.x;
+  pose_filtered.position.y = pose_filtered.position.y*k + (1.-k)*pose_robo1.pose.position.y;
+  pose_filtered.position.z = pose_filtered.position.z*k + (1.-k)*pose_robo1.pose.position.z;
+
+  transform.setOrigin(tf::Vector3(pose_filtered.position.x,
+				  pose_filtered.position.y,
+				  pose_filtered.position.z
+				   ) );
+  // Filter quaternion
+  pose_filtered.orientation.x = pose_filtered.orientation.x*k + (1.-k)*pose_robo1.pose.orientation.x;
+  pose_filtered.orientation.y = pose_filtered.orientation.y*k + (1.-k)*pose_robo1.pose.orientation.y;
+  pose_filtered.orientation.z = pose_filtered.orientation.z*k + (1.-k)*pose_robo1.pose.orientation.z;
+  pose_filtered.orientation.w = pose_filtered.orientation.w*k + (1.-k)*pose_robo1.pose.orientation.w;
+
+  quat = tf::Quaternion(pose_filtered.orientation.x,
+			pose_filtered.orientation.y,
+			pose_filtered.orientation.z,
+			pose_filtered.orientation.w);
+
   transform.setRotation(quat);
-  mocap_br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "mocap_world","world"));
+  
+  // TODO change to real time stamp
+  //mocap_br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "mocap_world","world"));
+  mocap_br.sendTransform(tf::StampedTransform(transform, pose_robo1.header.stamp, "mocap_world","world"));
+  //mocap_br.sendTransform(tf::StampedTransform(transform, ros::Time(0), "mocap_world","world")); // wrong is zero time
 
-  ROS_INFO("I heard something");
+  ROS_INFO("MOCAP message recieved.");
 }
-
 
 int main(int argc, char **argv)
 {
@@ -49,40 +67,27 @@ int main(int argc, char **argv)
 
   ros::NodeHandle n;
 
-  ros::Publisher tf_world_pub = n.advertise<std_msgs::String>("tf_world", 1000);
-
   //ros::Subscriber sub = n.subscribe("/object_1/pose", 10, &obj1_callback);
   ros::Subscriber sub = n.subscribe("/robot/pose", 10, &obj1_callback);
-
-  static tf::TransformBroadcaster obs2_br;
-
   static tf::TransformBroadcaster word_mocap_br;
 
-  //ros::Publisher tf_obs1_pub = n.advertise<std_msgs::String>("tf", 1000);
-  //os::Publisher tf_obs2_pub = n.advertise<std_msgs::String>("tf", 1000);
+  ros::Rate loop_rate(1);
 
-  ros::Rate loop_rate(10);
+  ros::spin();
 
   int count = 0;
   while (ros::ok())
   {
-    std_msgs::String msg;
-
-    std::stringstream ss;
-    ss << "Message count - " << count;
-    msg.data = ss.str();
-
-    ROS_INFO("%s", msg.data.c_str());
-    tf_world_pub.publish(msg);
-
     //br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "world", "mocap_world"));
-    
     ros::spinOnce();
+
+    std::cout << "spin \n";
+
     loop_rate.sleep(); // constant speed
+
     ++count;  // increment coutner
 
   }
-
 
   return 0;
 }
